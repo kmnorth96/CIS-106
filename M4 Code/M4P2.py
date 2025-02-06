9d7d5fdfd5298fed504becc078710a02fd894a1d
Keaton_Northcott <110203505+kmnorth96@users.noreply.github.com>
2025-02-06 14:54:16 -0600

    Delete M4 Code directory

--- M4 Code/M4P2.py
+++
@@ -1,7 +0,0 @@
-purchase_price = input("Enter the purchase price per share: ")
-stock_price = input("Enter the current stock price: ")
-quantity = input("Enter the number of shares: ")
-
-compute_value = (float(stock_price) - float(purchase_price)) * float(quantity)
-
-print("Value:{:.2f}".format(compute_value))
--- M4 Code/M4P4.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter first name: ")
-steps = float(input("Enter number of steps: "))
-
-calories = steps * 0.25
-
-print(name, "burned", calories, "calories.")
